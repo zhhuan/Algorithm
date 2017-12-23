a2e3b3824d08c9b112fdf6845eb843dd47496871
Johan <huan_zhong2@126.com>
2017-12-24 02:04:39 +0800

    利用遗传算法求解生产物流配送方案整体完成，还需添加图形化、测试代码

--- MachineLearn/GA.py
+++ MachineLearn/GA.py
@@ -26,6 +26,7 @@ sareas = [24,22,26,33,5,5,5,5,19,41,18,31,24]
 
 class GeneticAlgorithm(object):
     def __init__(self, genetics):
+        self.genetics = genetics
         pass
 
     def run(self):
@@ -36,7 +37,8 @@ class GeneticAlgorithm(object):
                 break
             population = self.next(fits_pops)
             pass
-        return population
+        fits_pops = [(self.genetics.fitness(chromo), chromo) for chromo in population]
+        return fits_pops
 
     def next(self, fits):
         parents_generator = self.genetics.parents(fits)
@@ -47,8 +49,8 @@ class GeneticAlgorithm(object):
             cross = random.random() < self.genetics.probability_crossover()
             children = self.genetics.crossover(parents) if cross else parents  #小于交叉概率，做交叉
             for chrom in children:
-                mutate = random.random() < self.genetics.probability_muttation()
-                nexts.append(self.genetics.mutation(chrom) if mutate else ch)
+                mutate = random.random() < self.genetics.probability_mutation()
+                nexts.append(self.genetics.mutation(chrom) if mutate else chrom)
                 pass
             pass
         return nexts[0:size]
@@ -56,9 +58,9 @@ class GeneticAlgorithm(object):
 
 class Logistics(object):
     """docstring for logistics"""
-    def __init__(self,generations=50,size=100,prob_crossover=0.75,prob_mutation=0.3):
+    def __init__(self,config=[13,6,3],generations=120,size=100,prob_crossover=0.75,prob_mutation=0.3):
         self.counter = 0
-
+        self.config = config
         self.generations = generations
         self.size = size
         self.prob_crossover = prob_crossover
@@ -70,12 +72,16 @@ class Logistics(object):
     def probability_mutation(self):
         return self.prob_mutation
 
-    def initial(self,stash_num,market_num,veh_num):
-        return [self.random_chromo(stash_num,market_num,veh_num)]
+    def initial(self):
+        stash_num = self.config[0]
+        market_num = self.config[1]
+        veh_num = self.config[2]
+        return [self.random_chromo(stash_num,market_num,veh_num) for j in range(self.size)]
 
     def fitness(self, chromo):
         #计算适应度,适应度计算的规则为每条配送路径要满足题设条件，并且目标函数即车辆行驶的时间越少，适应度越高
         flag = 1 
+        drive_time = 0
         transfers = get_transfers(chromo)
         flag = judge_over_area(transfers)
         for transfer in transfers:
@@ -88,8 +94,11 @@ class Logistics(object):
         return fitness
 
     def check_stop(self, fits_populations):
-        self.counter += 1
-        return self.counter >= self.generations
+        for chromo in fits_populations:
+            if chromo[0] < 2610:
+                self.counter += 1
+                return self.counter >= self.generations
+        return False
 
     def parents(self, fits_populations):
         while True:
@@ -101,12 +110,21 @@ class Logistics(object):
 
     def crossover(self, parents):
         """breed children"""
-        return parents
+        father,mother = parents
+        index1 = random.randrange(len(father))
+        index2 = random.randrange(len(mother))
+        if index1 > index2: index1, index2 = index2, index1
+        child1 = father[:index1] + mother[index1:index2] + father[index2:]
+        child2 = mother[:index1] + father[index1:index2] + mother[index2:]
+        return (child1, child2)
 
-    def mutation(self, chromosome):
+    def mutation(self, chromo):
         """mutate chromosome"""
-        return chromosome
-
+        index = random.randrange(len(chromo))
+        vary = [random.randrange(self.config[2]),random.randrange(self.config[1])]
+        mutated = list(chromo)
+        mutated[index] = vary
+        return mutated
 
     # internals
     def random_chromo(self,stash_num,market_num,veh_num):
@@ -118,103 +136,29 @@ class Logistics(object):
         return chromo
 
     def select(self, fits_populations):
-        sum_f = 0
-        for i in range(self.size):
-            sum_f += fits_populations[i][0]
-        p = [0] * self.size 
-        for i in range(self.size):
-            p[i] = 1 - (fits_populations[i][0] / sum_f)
-        q = [0] * self.size
-        for i in range(self.size):
-            s = 0
-            for j in range(0, i+1):
-                s += p[j]
-            q[i] = s
+            sum_f = 0
+            size = len(fits_populations)
+            for i in range(size):
+                sum_f += 1/fits_populations[i][0]
+            p = [0] * size 
+            for i in range(size):
+                p[i] =  (1 / fits_populations[i][0])/sum_f
+            q = [0] * size
+            for i in range(size):
+                s = 0
+                for j in range(0, i+1):
+                    s += p[j]
+                q[i] = s
 
-        v = []
-        for i in range(self.size):
-            r = random.random()
-            if r < q[0]:
-                return fits_populations[0][1]
-            for j in range(1,self.size):
-                if q[j-1] < r <= q[j]:
-                    return fits_populations[j][1]
+            v = []
+            for i in range(size):
+                r = random.random()
+                if r < q[0]:
+                    return fits_populations[0][1]
+                for j in range(1,size):
+                    if q[j-1] < r <= q[j]:
+                        return fits_populations[j][1]
 
-
-def get_transfers(chromo):
-    chromo_size = len(chromo)
-    transfers = []
-    for stash_num in range(chromo_size):
-        prev_transfer = None
-        if stash_num == 0 :
-            car_num = chromo[stash_num][0]
-            market_num = chromo[stash_num][1]
-            cur_transfer = Transfer(car_num,stash_num,market_num)
-            transfers.append(cur_transfer)
-        else:
-            car_num = chromo[stash_num][0]
-            market_num = chromo[stash_num][1]
-            for transfer in transfers:
-                if transfer.car.car_num == car_num:
-                    prev_transfer = transfer
-            cur_transfer = Transfer(car_num,stash_num,market_num,prev_transfer)
-            transfers.append(cur_transfer)
-    return transfers
-
-def judge_over_area(transfers):
-    flag = 1
-    area_couples = {}
-    for transfer in transfers:
-        market_num = transfer.departure.market_num
-        if not market_num in area_couple:
-            area_couples[market_num] = [transfer]
-        else:
-            area_couples[market_num].append(transfer)
-
-    for market_num in area_couples:
-        total_area = 0
-        com_transfers = area_couples[market_num]
-        com_market = Market(market_num)
-        for transfer in com_transfers:
-            stash_area = transfer.destination.stash_area
-            total_area += 5*stash_area
-        if total_area > com_market.market_area:
-            flag = 0
-    return flag
-    
-
-def parents(fits_populations):
-    while True:
-        father = select(fits_populations)
-        mother = select(fits_populations)
-        yield (father, mother)
-        pass
-    pass
-
-def select(fits_populations):
-        sum_f = 0
-        size = len(fits_populations)
-        for i in range(size):
-            sum_f += 1/fits_populations[i][0]
-        p = [0] * size 
-        for i in range(size):
-            p[i] =  (1 / fits_populations[i][0])/sum_f
-        print(p)
-        q = [0] * size
-        for i in range(size):
-            s = 0
-            for j in range(0, i+1):
-                s += p[j]
-            q[i] = s
-
-        v = []
-        for i in range(size):
-            r = random.random()
-            if r < q[0]:
-                return fits_populations[0][1]
-            for j in range(1,size):
-                if q[j-1] < r <= q[j]:
-                    return fits_populations[j][1]
 
 class Transfer(object):
     """docstring for Transfer"""
@@ -275,21 +219,79 @@ class Stash(object):
         self.stash_area = sareas[stash_num]
         
 
+def get_transfers(chromo):
+    chromo_size = len(chromo)
+    transfers = []
+    for stash_num in range(chromo_size):
+        prev_transfer = None
+        if stash_num == 0 :
+            car_num = chromo[stash_num][0]
+            market_num = chromo[stash_num][1]
+            cur_transfer = Transfer(car_num,stash_num,market_num)
+            transfers.append(cur_transfer)
+        else:
+            car_num = chromo[stash_num][0]
+            market_num = chromo[stash_num][1]
+            for transfer in transfers:
+                if transfer.car.car_num == car_num:
+                    prev_transfer = transfer
+            cur_transfer = Transfer(car_num,stash_num,market_num,prev_transfer)
+            transfers.append(cur_transfer)
+    return transfers
+
+def judge_over_area(transfers):
+    flag = 1
+    area_couples = {}
+    for transfer in transfers:
+        market_num = transfer.departure.market_num
+        if not market_num in area_couples:
+            area_couples[market_num] = [transfer]
+        else:
+            area_couples[market_num].append(transfer)
+
+    for market_num in area_couples:
+        total_area = 0
+        com_transfers = area_couples[market_num]
+        com_market = Market(market_num)
+        for transfer in com_transfers:
+            stash_area = transfer.destination.stash_area
+            total_area += 5*stash_area
+        if total_area > com_market.market_area:
+            flag = 0
+    return flag
+
+def fitness(chromo):
+    #计算适应度,适应度计算的规则为每条配送路径要满足题设条件，并且目标函数即车辆行驶的时间越少，适应度越高
+    flag = 1 
+    drive_time = 0
+    transfers = get_transfers(chromo)
+    flag = judge_over_area(transfers)
+    for transfer in transfers:
+        if transfer.arrival_time == 0:
+            flag = 0
+            break
+        drive_time += transfer.drive_time
+    
+    fitness = drive_time + 100000*(1-flag)
+    return fitness
+
+            
 if __name__ == '__main__':
     """solve logistics distribution problem"""
-    # logi = Logistics()
+    logistics = Logistics()
+    GA = GeneticAlgorithm(logistics)
+    populations = GA.run()
+    for chromo in populations:
+        print(chromo)
     # populations = logi.initial(13,6,3)
     # fits_pops = [(logi.fitness(chromo), chromo) for chromo in populations]
     # print(fits_pops)
-    fits_populations = [
-        [10000,[[0,0],[0,3],[1,1],[2,5],[2,2],[0,0],[2,2],[1,5],[1,2],[0,4],[2,0],[0,3],[1,2]]],
-        [2703,[[0,1],[0,3],[1,1],[2,5],[2,2],[0,0],[2,2],[1,5],[1,2],[0,4],[2,0],[0,3],[1,2]]],
-        [500,[[0,2],[0,3],[1,1],[2,5],[2,2],[0,0],[2,2],[1,5],[1,2],[0,4],[2,0],[0,3],[1,2]]],
-    ]
-
-    parents_generator = parents(fits_populations)
-    parents = next(parents_generator)
-    print(parents)
+    # chromo = [[0,0],[0,3],[1,1],[2,5],[2,2],[0,0],[2,2],[1,5],[1,2],[0,4],[2,0],[0,3],[1,2]]
+    # fitness = fitness(chromo)
+    # print(fitness)
+    # parents_generator = parents(fits_populations)
+    # parents = next(parents_generator)
+    # print(parents)
     
 
         
